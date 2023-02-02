43889aa20b80e0c8ee3188f45d2cbceb4c15967c
Hunter Dale <guyyatsu@gmail.com>
2023-02-02 18:17:50 -0500

    Add command-line functionality to the telegram bot.

--- BrokerBot.py
+++ BrokerBot.py
@@ -1,62 +1,64 @@
 import telepot
 from time import sleep
+import argparse
 from CredentialManagement import CredentialManagement
 from tools.AccountData import AccountEnumeration
-from alpaca.trading.client import TradingClient
-
-
-""" CREDENTIALS """
-keyfile = "/server/administrator/.credentials/.key"
-database = "/server/administrator/.credentials/credentials.db"
-
-# Gather the administrators telegram user id so the bot only responds to him.
-telegramID =  CredentialManagement.SingleKeyAPICredentials(
-  platform="telegram_admin",
-  credabase=database,
-  keyfile=keyfile
-)
-
-# Initialize the Telegram bot with the platforms API key.
-bot = telepot.Bot(
-  CredentialManagement.SingleKeyAPICredentials(
-    platform="telegram",
-    credabase=database,
-    keyfile=keyfile
-  )
-)
-
-# Unlock the Alpaca.Markets API key and secret.
-credentials = CredentialManagement.MultiKeyAPICredentials(
-  platform="alpaca", credabase=database, keyfile=keyfile
-)
-
-# Pull the credentials from the dictionary.
-key = credentials['key']; secret = credentials['secret']
-
-
-# Initialize the trade client with key and secret.
-brokerage_account = AccountData(TradingClient(key, secret, paper=False))
-
-
-""" End-User Interface """
-def BrokerBot(message):
-  """
-  The Broker Bot manages communications between the user and the daemon.
-  """
-  packet_data = message["text"].split(" ")
-  command = str(packet_data[0])
-
-  # Get all account data.
-  if command == "/account": bot.sendMessage(telegramID, brokerage_account.GetAccount())
-
-  # Enumerate all current positions. 
-  if command == "/positions": bot.sendMessage(telegramID, brokerage_account.GetAllPositions())
-
-  # List all assets offered by the broker.
-  if command == "/getassets": bot.sendMessage(telegramID, brokerage_account.ListAssets())
 
 
 if __name__ == "__main__":
-  bot.message_loop(BrokerBot)
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-K", "--keyfile", help="Designate a file containing an ssh key for encryption.")
+  parser.add_argument("-D", "--database", help="Designate a sqlite3 database file.")
+
+  arguments = parser.parse_args()
+
+
+  """ CREDENTIALS """
+  if arguments.keyfile: keyfile = arguments.keyfile
+  else: keyfile = "/server/administrator/.credentials/.key"
+
+  if arguments.database: database = arguments.database
+  else: database = "/server/administrator/.credentials/credentials.db"
+
+  # Gather the administrators telegram user id so the bot only responds to him.
+  telegramID =  CredentialManagement.SingleKeyAPICredentials(
+    platform="telegram_admin",
+    credabase=database,
+    keyfile=keyfile
+  )
+ 
+  # Initialize the Telegram bot with the platforms API key.
+  bot = telepot.Bot(
+    CredentialManagement.SingleKeyAPICredentials(
+      platform="telegram",
+      credabase=database,
+      keyfile=keyfile
+    )
+  )
+
+
+  """ End-User Interface """
+  def BrokerBot(message, keyfile, database):
+    """
+    The Broker Bot manages communications between the user and the daemon.
+    """
+  
+    brokerage_account = AccountEnumeration(keyfile, database)
+  
+    packet_data = message["text"].split(" ")
+    command = str(packet_data[0])
+  
+    # Get all account data.
+    if command == "/account": bot.sendMessage(telegramID, brokerage_account.GetAccount())
+  
+    # Enumerate all current positions. 
+    if command == "/positions": bot.sendMessage(telegramID, brokerage_account.GetAllPositions())
+  
+    # List all assets offered by the broker.
+    if command == "/getassets": bot.sendMessage(telegramID, brokerage_account.ListAssets())
+
+
+  bot.message_loop(BrokerBot, keyfile)
   while True:
     sleep(1)
