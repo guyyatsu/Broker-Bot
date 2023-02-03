a41bc7aa0da41cb1f49ccd1437df9633a179fabf
Hunter Dale <guyyatsu@gmail.com>
2023-02-03 16:32:56 -0500

    Live and runninggit add .

--- BrokerBot.py
+++ BrokerBot.py
@@ -1,5 +1,6 @@
 import telepot
 from time import sleep
+import logging
 import argparse
 from CredentialManagement import CredentialManagement
 from AlpacaAccountData.AccountData import AccountEnumeration
@@ -13,13 +14,19 @@ if __name__ == "__main__":
 
   arguments = parser.parse_args()
 
+  logfile = "/server/administrator/logs/Broker-Bot.log"
+  with open(logfile, 'w') as clearing_log: clearing_log.write("")
+  logging.basicConfig(filename=logfile, level=logging.DEBUG)
+
 
   """ CREDENTIALS """
   if arguments.keyfile: keyfile = arguments.keyfile
   else: keyfile = "/server/administrator/.credentials/.key"
+  logging.info(f"Keyfile: {keyfile}")
 
   if arguments.database: database = arguments.database
   else: database = "/server/administrator/.credentials/credentials.db"
+  logging.info(f"Database: {database}")
 
   # Gather the administrators telegram user id so the bot only responds to him.
   telegramID =  CredentialManagement.SingleKeyAPICredentials(
@@ -29,6 +36,7 @@ if __name__ == "__main__":
   )
  
   # Initialize the Telegram bot with the platforms API key.
+  logging.debug(f"Creating Telegram bot.")
   bot = telepot.Bot(
     CredentialManagement.SingleKeyAPICredentials(
       platform="telegram",
@@ -36,6 +44,7 @@ if __name__ == "__main__":
       keyfile=keyfile
     )
   )
+  logging.debug(f"Bot\n{bot}\nCreated.")
 
 
   """ End-User Interface """
@@ -44,10 +53,14 @@ if __name__ == "__main__":
     The Broker Bot manages communications between the user and the daemon.
     """
   
+    logging.debug("Contacting Alpaca brokerage.")
     brokerage_account = AccountEnumeration(keyfile, database)
+    logging.debug(f"Got details for brokerage.")
   
+    logging.debug("Recieving incoming transmission:")
     packet_data = message["text"].split(" ")
     command = str(packet_data[0])
+    logging.debug(f"Recieved command: {command}")
   
     # Get all account data.
     if command == "/account": bot.sendMessage(telegramID, brokerage_account.GetAccount())
@@ -56,7 +69,7 @@ if __name__ == "__main__":
     if command == "/positions": bot.sendMessage(telegramID, brokerage_account.GetAllPositions())
   
     # List all assets offered by the broker.
-    if command == "/getassets": bot.sendMessage(telegramID, brokerage_account.ListAssets())
+    if command == "/assets": bot.sendMessage(telegramID, brokerage_account.ListAssets())
 
 
   bot.message_loop(BrokerBot)
